418f0dfa4bc72d9bb07591562bcba3bea2c789e7
bushalin <hasibulhasan2009@gmail.com>
2021-03-05 15:19:07 +0900

    adding login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './account/login/login.component';
 import { AuthCallbackComponent } from './auth-callback/auth-callback.component';
 
 const routes: Routes = [
   { path: 'auth-callback', component: AuthCallbackComponent},
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
